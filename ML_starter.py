905266d4422474467272fb58ec07d771ea5ca5e3
Ming-hia <lukehuang201@hotmail.com>
2017-07-27 18:53:14 +0800

    Add files via upload

---
+++ ML_starter.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 27 14:56:34 2017
+
+@author: mingxia.huang
+"""
+
+import pandas as pd
+import numpy as np
+import lightgbm as lgb
+from random import sample
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+
+aisles = pd.read_csv("./downloads/aisles.csv")
+print "finish reading aisles.csv;"
+departments = pd.read_csv("./downloads/departments.csv")
+print "finish reading departments.csv;"
+prior = pd.read_csv("./downloads/order_products__prior.csv")
+print "finish reading order_products__prior.csv;"
+train = pd.read_csv("./downloads/order_products__train.csv")
+print "finish reading order_products__train.csv;"
+orders = pd.read_csv("./downloads/orders.csv")
+print "finish reading orders.csv;"
+products = pd.read_csv("./downloads/products.csv")
+print "finish reading products.csv.\n"
+
+train = pd.merge(train, orders)
+prior = pd.merge(prior, orders)
+test_orders = orders[orders.eval_set == "test"]
+
+user_product_list = prior[["user_id","product_id"]].drop_duplicates()
+test = pd.merge(test_orders, user_product_list, on = "user_id", how = "left")
+
+features = ["days_since_prior_order", "order_dow"]
+
+def k_fold(index, k = 5):
+    n = len(index)
+    index = set(index)
+    pool = index
+    s = n / k
+    index_list = []
+    for i in range(k):
+        if i < (k - 1):
+            ids = sample(pool, s)
+        else:
+            ids = pool
+        index_list.append({"train": index.difference(ids), "valid": set(ids)})
+        pool = pool.difference(ids)
+    return index_list
+            
+user_splits = k_fold(train.user_id.unique(), 5)
+
+X_test = test[features]
+test["pred"] = 0
+for i in range(1):
+    X_train = train[train.user_id.isin(user_splits[0]["train"])][features]
+    Y_train = train[train.user_id.isin(user_splits[0]["train"])]["reordered"]
+    X_valid = train[train.user_id.isin(user_splits[0]["valid"])][features]
+    Y_valid = train[train.user_id.isin(user_splits[0]["valid"])]["reordered"]
+    model = lgb.LGBMClassifier(objective='binary',
+                        max_depth = 4,
+                        learning_rate=0.05,
+                        n_estimators=100)                        
+    model.fit(X_train, Y_train, verbose = 10)
+    print "local score:", f1_score(Y_valid, model.predict(X_valid))
+    test["pred"] += model.predict(X_test)
+
+test["pred"] = np.where(test.pred > 0, 1, 0)
+prediction = test[["order_id", "product_id", "pred"]]
+prediction.product_id = prediction.product_id.apply(str)
+order_cnt = prediction.groupby("order_id").pred.sum()
+order_cnt = order_cnt[order_cnt == 0].reset_index()
+order_cnt["products"] = "None"
+empty = order_cnt[["order_id", "products"]]
+prediction = prediction[prediction.pred == 1][["order_id", "product_id"]]
+output = prediction[["order_id","product_id"]].groupby("order_id")["product_id"].apply(list).to_frame("products")
+output["products"] = output["products"].apply(lambda x: " ".join(x))
+output = pd.concat([output.reset_index(), empty])
+output.to_csv("./outputs/prediction.csv", index = False)
+print "complete."
